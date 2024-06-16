91e1de6118a5e88bf8875ac4b2d664b63b95e3db
kuznetsov-kostantin31 <kuznetsovkonst31@gmail.com>
2024-06-16 17:21:11 +1000

    Petcher-back v0.3.0

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import * as process from "process";
 import {NestFactory} from "@nestjs/core";
 import {AppModule} from "./app.module";
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {JwtAuthGuard} from "./auth/jwt-auth.guard";
+import {ValidationPipe} from "./pipes/validation.pipe";
 
 
 async function start(){
@@ -9,14 +11,15 @@ async function start(){
     const app = await NestFactory.create(AppModule)
 
     const config = new DocumentBuilder()
-        .setTitle('Бэкенд проект')
-        .setDescription('Тестовая часть')
-        .setVersion('1.0.0')
-        .addTag('AIZEX')
+        .setTitle('PETCHER PROJECT')
+        .setDescription('Бэкенд часть Petcher project')
+        .setVersion('0.3.0')
+        .addTag('Petcher Team')
         .build()
     const document = SwaggerModule.createDocument(app, config)
     SwaggerModule.setup('/api/docs', app, document)
 
+    app.useGlobalPipes(new ValidationPipe())
     await app.listen(PORT, ()=>console.log(`Server started on port: ${PORT}`))
 }
 
